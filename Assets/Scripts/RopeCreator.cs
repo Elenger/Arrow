dd117688ac2b66308e80d25a799aa742fc501347
Elenger <orion96king@gmail.com>
2019-11-29 02:05:55 +0200

    Add final project with colliders

--- Assets/Scripts/RopeCreator.cs
+++ Assets/Scripts/RopeCreator.cs
@@ -7,7 +7,6 @@ public class RopeCreator : MonoBehaviour
     [SerializeField] private GameObject p_RopePrefab;
     private int p_NumberArrowLayer = 10;
     private bool p_ArrowFromTree = false;
-    private CharacterController p_characterController;
     private Transform p_TreeTransform;
     private Transform p_ArrowTrasform;
     private Vector3 p_RopePosition;
@@ -28,24 +27,25 @@ public class RopeCreator : MonoBehaviour
             p_ArrowFromTree = collision.gameObject.GetComponent<Arrow>().arrowFromTree;
             p_ArrowTrasform = collision.gameObject.GetComponent<Arrow>().transform;
             p_TreeTransform = collision.gameObject.GetComponent<Arrow>().treeTransform;
-            p_characterController = collision.gameObject.GetComponent<Arrow>().characterController;
         }
     }
 
     private void RopeCreate()
     {
         p_RopePosition = RopePosition();
-        float lenghtRope = (p_ArrowTrasform.position - p_TreeTransform.position).magnitude;
+        float lenghtRope = (p_ArrowTrasform.position - p_TreeTransform.position).magnitude + 1;
         GameObject newRope = Instantiate(p_RopePrefab) as GameObject;
         newRope.transform.position = p_RopePosition;
         Vector3 scaleRope = newRope.transform.localScale;
-        scaleRope.z = lenghtRope;
+        scaleRope.x = lenghtRope;
         newRope.transform.localScale = scaleRope;
-        Quaternion rotationRope = Quaternion.LookRotation(p_ArrowTrasform.position - p_TreeTransform.position);
-        newRope.transform.rotation = rotationRope;
 
-        /*float angleRad = Mathf.Atan2(p_ArrowTrasform.position.y - p_TreeTransform.position.y, p_ArrowTrasform.position.x - p_TreeTransform.position.x);
-        float angleDeg = angleRad * Mathf.Rad2Deg;*/
+        float angleRad = Mathf.Atan2(p_ArrowTrasform.position.y - p_TreeTransform.position.y, p_ArrowTrasform.position.x - p_TreeTransform.position.x);
+        float angleDeg = angleRad * Mathf.Rad2Deg;
+        Quaternion angleRotation = new Quaternion();
+        angleRotation.eulerAngles = new Vector3(0f,0f, angleDeg);
+        newRope.transform.rotation = angleRotation;
+        // Quaternion rotationRope = Quaternion.LookRotation(p_ArrowTrasform.position - p_TreeTransform.position);
     }
 
     private Vector3 RopePosition()
